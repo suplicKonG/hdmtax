d199620cb565b9cf551b92f39317aaa7e1e21026
suplicKonG <skuplik.s@seznam.cz>
2025-04-20 23:37:51 +0200

    Update

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -2,18 +2,6 @@ import './index.css'
 import { useState, useEffect } from "react";
 
 
-useEffect(() => {
-  const setVH = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
-
-  setVH();
-  window.addEventListener('resize', setVH);
-
-  return () => window.removeEventListener('resize', setVH);
-}, []);
-
 type NavProps = {
   isFooter?: boolean;
   className?: string;
